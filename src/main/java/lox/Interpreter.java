654e609a844a06222d4c164717dc4e92df47c620
Joel D'Autilio <joeldautilio@gmail.com>
2023-11-27 22:32:00 -0300

    Logical operators

--- src/main/java/lox/Interpreter.java
+++ src/main/java/lox/Interpreter.java
@@ -205,4 +205,17 @@ class Interpreter implements Expr.Visitor<Object>,
         return null;
     }
 
+    @Override
+    public Object visitLogicalExpr(Expr.Logical expr) {
+        Object left = evaluate(expr.left);
+
+        if (expr.operator.type == TokenType.OR) {
+            if (isTruthy(left)) return left;
+        } else {
+            if (!isTruthy(left)) return left;
+        }
+
+        return evaluate(expr.right);
+    }
+
 }
